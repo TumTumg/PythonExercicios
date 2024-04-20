abf864821cc51e26405b9149db12f4fe395bb01c
DANIEL ZAMBOTTI LIMA <daniel.zlima1@senacsp.edu.br>
2024-04-19 21:57:11 -0300

    Primeiro Commit

--- Control.py
+++ Control.py
@@ -12,11 +12,19 @@ class Control:
 
     def menu(self):
         self.opcao = int(input("-----Menu-----\n\n" +
-                              "\n0.Sair"         +
-                              "\n1. Somar"       +
-                              "\n2.Subtrair"     +
-                              "\n3. Dividir"     +
-                              "\n4. Multiplicar" +
+                              "\n0.Sair"          +
+                              "\n1. Somar"        +
+                              "\n2. Subtrair"     +
+                              "\n3. Dividir"      +
+                              "\n4. Multiplicar"  +
+                              "\n5. Tabuada"      +
+                              "\n6. Contando até dez!!!!" +
+                              "\n7. Pares até vinte!" +
+                              "\n8. Soma dos números até cem" +
+                              "\n9. Múltiplos de 5 até 50" +
+                              "\n10. Número digitado é par ou impar?" +
+                              "\n11. Número digitado é negativo, positivo ou zero?" +
+                              "\n12. Somando do 1 até o número digitado!" +
                               "\nEscolha uma das opções acima: "))
 
 
@@ -31,11 +39,31 @@ class Control:
             elif(self.opcao == 2):
                 self.coletar()
                 print(self.exer.subtrair())
-            elif (self.opcao == 3):
+            elif(self.opcao == 3):
                 self.coletar()
                 print(self.exer.dividir())
-            elif (self.opcao == 4):
+            elif(self.opcao == 4):
                 self.coletar()
                 print(self.exer.multiplicar())
+            elif(self.opcao == 5):
+                self.coletar()
+                print(self.exer.tabuada())
+            elif (self.opcao == 6):
+                print(self.exer.ateDez())
+            elif (self.opcao == 7):
+                print(self.exer.paresUmAVinte())
+            elif (self.opcao == 8):
+                print(self.exer.somaAteCem())
+            elif (self.opcao == 9):
+                print(self.exer.multiploCinco())
+            elif (self.opcao == 10):
+                self.coletar()
+                print(self.exer.parImpar())
+            elif (self.opcao == 11):
+                self.coletar()
+                print(self.exer.posNegZero())
+            elif (self.opcao == 12):
+                self.coletar()
+                print(self.exer.somaAteDig())
             else:
                 print("Código escolhido não é válido!")
--- Exercicios.py
+++ Exercicios.py
@@ -3,6 +3,9 @@ class Exercicios:
         #Construtor
         self.num1 = 0
         self.num2 = 0
+        self.tabNum1 = ""
+        self.tabNum2 = ""
+        self.i = 0
 
     def somar(self):
         return f"{self.num1} + {self.num2} = {self.num1 + self.num2}"
@@ -18,3 +21,60 @@ class Exercicios:
 
     def multiplicar(self):
         return f"{self.num1} * {self.num2} = {self.num1 * self.num2}"
+
+    def tabuada(self):
+        for self.i in range(0, 11, 1):  # Inicio, fim, contagem) | i = contador
+            self.tabNum1 += f"{self.num1} * {self.i} = {self.num1 * self.i}\n"
+            self.tabNum2 += f"{self.num2} * {self.i} = {self.num2 * self.i}\n"
+        return f"Tabuada do {self.num1}: \n{self.tabNum1} \n\n Tabuada do {self.num2}: \n{self.tabNum2}"
+
+    def ateDez(self):
+        for self.i in range(1, 11):
+            print(self.i)
+
+    def paresUmAVinte(self):
+        for self.i in range(0, 21, 2):
+            print(self.i)
+
+    def somaAteCem(self):
+        self.soma = 0
+        for self.i in range(1, 101):
+            self.soma += self.i
+        return f"\nA soma dos números de 1 a 100 é: {self.soma}"
+
+    def multiploCinco(self):
+        for self.i in range(5, 55, 5):
+            print(self.i)
+
+    def parImpar(self):
+        if (self.num1 % 2 == 0):
+            self.resultado1 = f"O número {self.num1} é par."
+        else:
+            self.resultado1 = f"O número {self.num1} é ímpar."
+        if (self.num2 % 2 == 0):
+            self.resultado2 = f"O número {self.num2} é par."
+        else:
+            self.resultado2 = f"O número {self.num2} é ímpar."
+        return self.resultado1, self.resultado2
+
+    def posNegZero(self):
+        if (self.num1 == 0):
+            self.resultado1 = f"O número digitado: {self.num1} é zero"
+        elif (self.num1 > 0):
+            self.resultado1 = f"O número digitado: {self.num1} é positivo"
+        else:
+            self.resultado1 = f"O número digitado: {self.num1} é negativo"
+        if (self.num2 == 0):
+            self.resultado2 = f"O número digitado: {self.num2} é zero"
+        elif (self.num2 > 0):
+            self.resultado2 = f"O número digitado: {self.num2} é positivo"
+        else:
+            self.resultado2 = f"O número digitado: {self.num2} é negativo"
+        return self.resultado1, self.resultado2
+
+    def somaAteDig(self):
+        self.soma = 0
+        for self.i in range(1, self.num1):
+            self.soma += self.i
+        return f"\nA soma dos números de 1 à: {self.num1} é: {self.soma}"
+
