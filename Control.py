1b14e4e7e4738bb2a9b3612ac75a56d78813e030
DANIEL ZAMBOTTI LIMA <daniel.zlima1@senacsp.edu.br>
2024-05-03 20:10:15 -0300

    Segundo commit

--- Control.py
+++ Control.py
@@ -9,6 +9,8 @@ class Control:
         self.exer.num1 = int(input("Informe o primeiro número: "))
         self.exer.num2 = int(input("Informe o segundo número: "))
 
+    def coletarApenasUm(self):
+        self.exer.num1 = int(input("Informe um número: "))
 
     def menu(self):
         self.opcao = int(input("-----Menu-----\n\n" +
@@ -24,9 +26,20 @@ class Control:
                               "\n9. Múltiplos de 5 até 50" +
                               "\n10. Número digitado é par ou impar?" +
                               "\n11. Número digitado é negativo, positivo ou zero?" +
-                              "\n12. Somando do 1 até o número digitado!" +
+                              "\n12. Indo até o número digitado" +
+                              "\n13. Somando até o número digitado" +
+                              "\n14. Números primos até 20" +
+                              "\n15. Número digitado é primo?" +
+                              "\n16. Fatorial do número digitado" +
+                              "\n17. Imprimir Fibonacci" +
+                              "\n18. Número digitado é termo Fibonacci?" +
+                              "\n19. Soma dos digitos dos números" +
+                              "\n20. Pares e impares até o nímero digtado" +
+                              "\n21. Números primos até o nímero digitado:" +
+                              "\n22. Sequência de Collatz" +
+                              "\n23. Soma dos números pares e ímpares até um número" +
+                              "\n24. Verificar se um número é perfeito" +
                               "\nEscolha uma das opções acima: "))
-
 
     def operacao(self):
         while(self.opcao != 0):
@@ -63,7 +76,41 @@ class Control:
                 self.coletar()
                 print(self.exer.posNegZero())
             elif (self.opcao == 12):
-                self.coletar()
+                self.coletarApenasUm()
+                print(self.exer.ateDig())
+            elif (self.opcao == 13):
+                self.coletarApenasUm()
                 print(self.exer.somaAteDig())
+            elif (self.opcao == 14):
+                 print(self.exer.imprimirPrimos())
+            elif (self.opcao == 15):
+                self.coletarApenasUm()
+                print(self.exer.verificandoPrimo())
+            elif (self.opcao == 16):
+                self.coletarApenasUm()
+                print(self.exer.calcularFatorial())
+            elif (self.opcao == 17):
+                 print(self.exer.sequenciaFibonacci())
+            elif (self.opcao == 18):
+                self.coletarApenasUm()
+                print(self.exer.verificarFibonacci())
+            elif (self.opcao == 19):
+                self.coletarApenasUm()
+                print(self.exer.calcularSomaDigitos())
+            elif (self.opcao == 20):
+                self.coletarApenasUm()
+                print(self.exer.DigParesImpares())
+            elif (self.opcao == 21):
+                self.coletarApenasUm()
+                print(self.exer.DigPrimosAte())
+            elif (self.opcao == 22):
+                self.coletarApenasUm()
+                print(self.exer.sequenciaCollatz())
+            elif (self.opcao == 23):
+                self.coletarApenasUm()
+                print(self.exer.somaParesImpares())
+            elif (self.opcao == 24):
+                self.coletarApenasUm()
+                print(self.exer.numeroPerfeito())
             else:
                 print("Código escolhido não é válido!")
--- Exercicios.py
+++ Exercicios.py
@@ -7,6 +7,7 @@ class Exercicios:
         self.tabNum2 = ""
         self.i = 0
 
+
     def somar(self):
         return f"{self.num1} + {self.num2} = {self.num1 + self.num2}"
 
@@ -72,9 +73,147 @@ class Exercicios:
             self.resultado2 = f"O número digitado: {self.num2} é negativo"
         return self.resultado1, self.resultado2
 
+    def ateDig(self):
+        for self.i in range(1, self.num1 + 1):
+            print(self.i)
+
     def somaAteDig(self):
         self.soma = 0
-        for self.i in range(1, self.num1):
+        for self.i in range(1, self.num1 + 1):
             self.soma += self.i
-        return f"\nA soma dos números de 1 à: {self.num1} é: {self.soma}"
+        return f"\nA soma dos números de 1 até {self.num1} é: {self.soma}"
+
+    def imprimirPrimos(self):
+        primos = []
+
+        for num in range(1, 21):
+            eh_primo = True
+            for i in range(2, num):
+                if num % i == 0:
+                    eh_primo = False
+                    break
+            if eh_primo:
+                primos.append(num)
+
+        return f"Os números primos de 1 à 20 são: {primos}"
+
+    def verificandoPrimo(self):
+        if self.num1 <= 1:
+            return f"{self.num1} não é primo."
+        elif self.num1 == 2:
+            return f"{self.num1} é primo."
+        else:
+            for self.i in range(2, self.num1):
+                if self.num1 % self.i == 0:
+                    return f"{self.num1} não é primo."
+            return f"{self.num1} é primo."
+
+    def calcularFatorial(self):
+        if self.num1 < 0:
+            return "O fatorial de números negativos não está definido."
+        elif self.num1 == 0:
+            return 1
+        else:
+            self.fatorial = 1
+            for self.i in range(1, self.num1 + 1):
+                self.fatorial *= self.i
+            return f"O número fatorial de {self.num1} é {self.fatorial}"
+
+    def sequenciaFibonacci(self):
+        # Inicializando os dois primeiros termos da sequência
+        self.termo1 = 0
+        self.termo2 = 1
+
+        # Imprimindo os dois primeiros termos
+        print(self.termo1)
+        print(self.termo2)
+
+        # Loop para calcular os próximos termos e imprimi-los
+        for self.i in range(2, 10):  # Vamos até o décimo termo, começando a contar a partir do zero
+            self.proximoTermo = self.termo1 + self.termo2
+            print(self.proximoTermo)
+
+            # Atualizando os valores dos termos para os próximos cálculos
+            self.termo1 = self.termo2
+            self.termo2 = self.proximoTermo
+
+    def verificarFibonacci(self):
+        a, b = 0, 1
+        while b < self.num1:
+            a, b = b, a + b
+        if b == self.num1:
+            return f"{self.num1} é um número de Fibonacci."
+        else:
+            return f"{self.num1} não é um número de Fibonacci."
+
+    def calcularSomaDigitos(self):
+        self.soma = 0
+        while self.num1:
+            self.digito = self.num1 % 10
+            self.soma += self.digito
+            self.num1 //= 10
+        return f"A soma dos digitos do número digitado é: {self.soma}"
+
+    def DigParesImpares(self):
+        pares = []
+        impares = []
+
+        for self.i in range(1, self.num1 + 1):
+            if self.i % 2 == 0:
+                pares.append(self.i)
+            else:
+                impares.append(self.i)
+
+        return pares, impares
+
+    def DigPrimosAte(self):
+        primos = []
+
+        for self.i in range(2, self.num1 + 1):
+            eh_primo = True
+            for j in range(2, int(self.i ** 0.5) + 1):
+                if self.i % j == 0:
+                    eh_primo = False
+                    break
+            if eh_primo:
+                primos.append(self.i)
+
+        return primos
+
+    def sequenciaCollatz(self):
+        # Função para imprimir a sequência de Collatz de um número
+        collatz_seq = [self.num1]
+        while self.num1 != 1:
+            if self.num1 % 2 == 0:
+                self.num1 //= 2
+            else:
+                self.num1 = self.num1 * 3 + 1
+            collatz_seq.append(self.num1)
+        return collatz_seq
+
+    def somaParesImpares(self):
+        # Função para calcular a soma dos números pares e ímpares até um número dado
+        soma_pares = 0
+        soma_impares = 0
+        for num in range(1, self.num1 + 1):
+            if num % 2 == 0:
+                soma_pares += num
+            else:
+                soma_impares += num
+        return soma_pares, soma_impares
+
+    def numeroPerfeito(self):
+        # Função para verificar se um número é perfeito
+        soma_divisores = sum([i for i in range(1, self.num1) if self.num1 % i == 0])
+        if soma_divisores == self.num1:
+            return f"O número {self.num1} é um número perfeito."
+        else:
+            return f"O número {self.num1} não é um número perfeito."
+
+
+
+
+
+
+
 
